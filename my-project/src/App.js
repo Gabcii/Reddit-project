af57c112c8773eb98596fe0037994585d5e1d610
Gabcii <gderzsi@yahoo.co.uk>
2024-04-02 14:06:24 +0100

    changes committed

---
+++ my-project/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Reddit Project</h1>
+      <input></input>
+      <p>Search</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-project/src/api/api.js
@@ -0,0 +1,1 @@
+const API_ROOT = 'https://www.reddit.com';
